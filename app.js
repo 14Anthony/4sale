238bda5929ff1fe96fdd94b95e512da2e7405330
Anthony Belcastro <belcastt@live.com>
2020-10-24 15:36:44 -0700

    I am going move these routes up into admin.js and shop.js,  I want a good commit, before I do so on this, as It may break and I want a clean copy of why

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const express = require('express');
 
 // const routes = require('./routes');
 const app = express();
-//------------------------------------------------------------------------------------
+//-----------------------ALWAYS RUNS--------------------------------------------
 app.use('/', (req, res, next) => {
     console.log("LANDING PAGE for ZACH?TRAVIS?FRANK?");
     // RD: making sure that the request continues to the next middleware in line
     next();
 });
-//-------------------------------------------------------------------------------------
+//-----------------------ADD PRODUCT ROUTE -------------------------------------
 app.use('/add-product', (req, res, next) => {
-    console.log("ADD PRODUCT PAGE FOR ZACH?TRAVIS?FRANK?")
-    res.send("<h3>HELLO REACT REPLACEMENTS ADD PRODUCT PAGE</h3>")
+    console.log("ADD PRODUCT PAGE FOR ZACH?TRAVIS?FRANK?");
+    res.send(' <form action="/product" method = "POST"><input type ="text" name="title"><button type="submit">ADD PRODUCT</button></input><form>');
 });
-//-------------------------------------------------------------------------------------
 
+//-------------------------PRODUCT ROUTE-----------------------------------------
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+});
 app.use("/...", (req, res, next) => {
     console.log("ANOTHER PAGE FOR ZACH?TRAVIS?FRANK?");
     res.send('<h3>ANOTHER REACT TEMPLATE FOR FRANK,TRAVIS,ZACHARIAH');
@@ -24,6 +28,6 @@ app.use("/...", (req, res, next) => {
 
 // console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+// const server = http.createServer(routes.handler);
 
 server.listen(3000);
