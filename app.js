f51a688809ca0c8313d96e00a1e9e551e00712ee
Anthony Belcastro <belcastt@live.com>
2020-10-24 14:54:28 -0700

    Landing page route, /, and add product route

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+// const routes = require('./routes');
+const app = express();
+//------------------------------------------------------------------------------------
+app.use('/', (req, res, next) => {
+    console.log("LANDING PAGE for ZACH?TRAVIS?FRANK?");
+    // RD: making sure that the request continues to the next middleware in line
+    next();
+})
+//-------------------------------------------------------------------------------------
+app.use('/add-product', (req, res, next) => {
+    console.log("ADD PRODUCT PAGE FOR ZACH?TRAVIS?FRANK?")
+    res.send("<h3>HELLO REACT REPLACEMENTS CHECK CONSOLE NOW</h3>")
+})
+//-------------------------------------------------------------------------------------
 
-console.log(routes.someText);
+
+
+// console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
 
