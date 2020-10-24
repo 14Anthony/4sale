0442b57af48a1add67e3c10b9455abe7f7f4a2a4
Anthony Belcastro <belcastt@live.com>
2020-10-24 15:02:44 -0700

    adding another template route for ZACHARIAH,FRANK, TRAVIS

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ app.use('/', (req, res, next) => {
     console.log("LANDING PAGE for ZACH?TRAVIS?FRANK?");
     // RD: making sure that the request continues to the next middleware in line
     next();
-})
+});
 //-------------------------------------------------------------------------------------
 app.use('/add-product', (req, res, next) => {
     console.log("ADD PRODUCT PAGE FOR ZACH?TRAVIS?FRANK?")
-    res.send("<h3>HELLO REACT REPLACEMENTS CHECK CONSOLE NOW</h3>")
-})
+    res.send("<h3>HELLO REACT REPLACEMENTS ADD PRODUCT PAGE</h3>")
+});
 //-------------------------------------------------------------------------------------
 
+app.use("/...", (req, res, next) => {
+    console.log("ANOTHER PAGE FOR ZACH?TRAVIS?FRANK?");
+    res.send('<h3>ANOTHER REACT TEMPLATE FOR FRANK,TRAVIS,ZACHARIAH');
+});
 
 
 // console.log(routes.someText);
